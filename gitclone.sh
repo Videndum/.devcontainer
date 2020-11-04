c74ad681663ada7d7b326d3a320653abe6af7597
Jonathan Stevens <jonathan@videndum.studio>
2020-11-04 23:45:28 +0000

    fix: ssh & shutdownAction

--- gitclone.sh
+++ gitclone.sh
@@ -2,7 +2,7 @@
 
 # Clone the repositories
 
-git clone https://github.com/Videndum/Smartcloud.git projects/smartcloud
-git clone https://github.com/Videndum/Universal-GitAction-Workflows.git projects/github-actions
-git clone https://github.com/Videndum/manage-github-secrets.git projects/github-secrets
-git clone https://github.com/Videndum/super-labeler-action.git projects/super-labeler-action
+git clone git@github.com:Videndum/Smartcloud.git projects/smartcloud
+git clone git@github.com:Videndum/Universal-GitAction-Workflows.git projects/github-actions
+git clone git@github.com:Videndum/manage-github-secrets.git projects/github-secrets
+git clone git@github.com:Videndum/super-labeler-action.git projects/super-labeler-action
