e9defdf388fff900aa7bfe06f4eba9977ca09240
Jonathan Stevens <jonathan@videndum.studio>
2020-11-30 10:32:15 +0000

    update: update clones and paths

--- gitclone.sh
+++ gitclone.sh
@@ -3,6 +3,4 @@
 # Clone the repositories
 
 git clone git@github.com:Videndum/Smartcloud.git projects/smartcloud
-git clone git@github.com:Videndum/Universal-GitAction-Workflows.git projects/github-actions
-git clone git@github.com:Videndum/manage-github-secrets.git projects/github-secrets
-git clone git@github.com:Videndum/super-labeler-action.git projects/super-labeler-action
+git clone git@github.com:Videndum/action-mastermind.git projects/action-mastermind
